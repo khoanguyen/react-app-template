55c4b1df8d714149e442903582cfceb6ef098861
KEEP <minhkhoa4783@gmail.com>
2019-03-11 00:14:49 -0700

    remove unused files

--- compile/public/js/lib/services.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var util_1 = __importDefault(require("./util"));
-exports.Util = util_1.default;
-var log_1 = __importDefault(require("./../../../common/log"));
-exports.Logger = log_1.default;
