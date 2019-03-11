6307066cf40ffc7544d0acc0edd23837c05f27d1
KEEP <minhkhoa4783@gmail.com>
2019-03-11 00:11:28 -0700

    remove unused files

--- src/public/js/main.tsx
+++ src/public/js/main.tsx
@@ -5,50 +5,19 @@ import $ from 'jquery';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './modules/home/home';
 
-
-
-interface IAppProps {
-    studentName?: string;
+interface IAppProps {    
 }
 
-interface IAppState {
-    student?: Student | null;
+interface IAppState {    
 }
 
 class App extends React.Component<IAppProps, IAppState> {
 
-    studentName: React.RefObject<HTMLInputElement>;
-
     constructor(props: any) {
-        super(props)
-
-        this.studentName = React.createRef();
-
-        this.state = {
-            student: this.props.studentName ? new Student(this.props.studentName) : null
-        };
-
-        this._onSay = this._onSay.bind(this);
+        super(props);
     }
 
-    /**
-     * @private
-     */
-    _onSay() {
-        const l_input = this.studentName.current;
-
-        if (l_input) {
-            const $input: JQuery<HTMLInputElement> = $(l_input);
-
-            this.setState({
-                student: l_input.value ? new Student(l_input.value) : null
-            });
-        }
-    }
-
-    render() {
-        const student = this.state.student;
-
+    render() {       
         return (
             <div className='app-main'>
                 {this.props.children}
